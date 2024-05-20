c0d2dabbea16908cd52898d14adc0fd915640241
potatoboss123 <jk32955@students.mcpasd.k12.wi.us>
2024-05-20 17:48:53 -0500

    Update levels.js
    um Neal, I added some of the h blocks in my level so just an fyi it might be worth checking this one over in case it breaks the game......

--- levels.js
+++ levels.js
@@ -1049,40 +1049,42 @@ const LEVELS = [
   "  ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ",
   "  =$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$=$= O",
   "=============================================================",
-],[
+],
+//Freaky Deeky	
+[
 "i    G                    iiiiii                                                   i",
 "i                                                                                  i",
-"i^	 ^									   										   										   								i",
-"i========  ^^== ===??===        ==  === = ===????===   =^=^==   =====   = =  ==== i",
+"i^      ^                                                                          i",
+"i========  ^^== ===??===        ==  === = ===????===   =^=^==   =====   = =  ====  i",
 "i   v  v           ii    ll  ll                i i                                  i",
 "i                        ?      G   G                                              i",
-"i										   										   										   								i",
-"i										   										   										   								i",
+"i                                                                                  i",
+"i                                                                                  i",
 "i      ==??==           ==l====                                                    i",
 "i        ii                                                                        i",
-"i										   										   										   								i",
+"i                                                                                  i",
 "i                            ^   G   G^                                             i",
 "i   ==?===    = == ===   i   =====i====iiii   =i====  l ==?== ? l ===l ===  ====   i",
 "i      i                                                                           i",
-"i										   										   										   								i",
-"i						   										   								i",
-"i										   										   										   								i",
+"i                                                                                  i",
+"i                                                                                  i",
+"i                                                                                  i",
 "iii?????i      ii??i       ii??????ii         ii????ii      i???ii         ii??ii",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i    ll                         ll  lll       ll   lll    ll            lll  ll    i",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
+"i                                                                                  i",
+"$   =====                              ^     l                ^",
+"=          =   =",
+"=          i  =            $                            =",
+"=         i   =^^^^^^^^^^^^=^^^^^^^^^^  ^^^^                       ", 
+"==?==                h      $       l                    l",
+"  ^         =     h               h",
+"           =             h      h",
+"           ===??===h=======    =   ==^==     ===              =",                             
+"i                                                                                  i",
 "i  ==??=  ===  == ==??==   =?=  ==?== ===   ==??= ==   ===   == ==??==   ==??=    = i",
-"i										   										   										   								i",
-"I	   										   								i",
-"i										   										   										   								i",
-"i										   										   										   								i",
+"i                                                                                  i",
+"i                                                                                  i",
+"i                                                                                   i",
+"i                                                                                  i",
 "i   ==i??==   ======  ??  ===== ^ = ^ =  =  ==??==   ===== ====  ==i==  ===??===ii i",
 "i                     ii                                                           i",
 "i                                                                           iiiiiiii",
@@ -1184,7 +1186,7 @@ const levelConf = {
     solid(),
     scale(1),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "ground"
   ],
   "?": () => [
@@ -1192,14 +1194,14 @@ const levelConf = {
     area(),
     scale(1),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "ground"
   ],
   "$": () => [
     sprite("coin"),
     area(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "coin",
   ],
   "^": () => [
@@ -1207,7 +1209,7 @@ const levelConf = {
     area(),
     solid(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "danger",
   ], 
   "v": () => [
@@ -1215,7 +1217,7 @@ const levelConf = {
     area(),
     solid(),
     origin("top"),
-     
+    offscreen({ destroy: true }),
     "danger",
   ],
   "O": () => [
@@ -1223,21 +1225,21 @@ const levelConf = {
     area({ scale: 0.5, }),
     pos(0, -12),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "portal",
   ],
   "l": () => [
     sprite("jumpy"),
     origin("bot"),
     area(),
-     
+    offscreen({ destroy: true }),
     "jumpy",
   ],
 "h": () => [
     sprite("invisdanger"),
     origin("bot"),
     area(),
-     
+    offscreen({ destroy: true }),
     "jumpy",
   ],
   "D": () => [
@@ -1245,14 +1247,14 @@ const levelConf = {
     area(),
     solid(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "door",
   ],
   "k": () => [
     sprite("key"),
     area(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "key",
   ],
   ">": () => [
@@ -1260,7 +1262,7 @@ const levelConf = {
     area(),
     solid(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "right",
   ],
   "<": () => [
@@ -1268,51 +1270,53 @@ const levelConf = {
     area(),
     solid(),
     origin("bot"),
-     
+    offscreen({ destroy: true }),
     "left",
   ],
   ".": () => [
     sprite("block"),
     area(),
     solid(),
-     
+    offscreen({ destroy: true }),
     origin("bot"),
   ],
   "{": () => [
     sprite("signpostl"),
     area(),
-     
+    offscreen({ destroy: true }),
     origin("bot"), 
   ],
     "}": () => [
     sprite("signpostr"),
     area(),
-     
+    offscreen({ destroy: true }),
     origin("bot"), 
   ],
     "-": () => [
     sprite("signposte"),
     area(),
+    offscreen({ destroy: true }),
     origin("bot"), 
   ],
     "i": () => [
     sprite("invisdanger"),
     area(),
     "danger",
+    offscreen({ destroy: true }),
     origin("bot"), 
   ],
   "S": () => [
     sprite("invisdanger"),
     area(),
     "spawn",
-     
+    offscreen({ destroy: true }),
     origin("bot"), 
   ],
   "Q": () => [
     sprite("invisdanger"),
     area(),
     "arg",
-     
+    offscreen({ destroy: true }),
     origin("bot"),
   ],
   "G": () => [
@@ -1322,11 +1326,13 @@ const levelConf = {
     origin("bot"),
     area(), 
     body(), 
+    offscreen({ destroy: true }),
     ],
   "!": () => [
     sprite("darrow"),
     origin("bot"),
     area(),
+    offscreen({ destroy: true }),
   ],
   
 }
