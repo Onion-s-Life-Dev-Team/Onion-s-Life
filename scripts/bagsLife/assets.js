d69a677fb0af63bd6b0fdaf03bcba32d0667cea2
Eli O'Connor <94636503+Eli-code1@users.noreply.github.com>
2024-06-17 22:25:31 +0000

    Fix up stuff for bags life

---
+++ scripts/bagsLife/assets.js
@@ -0,0 +1,16 @@
+export function loadAssets(){
+    // load assets
+    loadSprite("bag", "assets/sprites/bag.png")
+    loadSprite("snow", "assets/sprites/snow.png")
+    loadSprite("coin","assets/sprites/coin.png")
+
+    loadSprite("portal", "assets/sprites/portal.png")
+
+    loadSprite("spike", "assets/sprites/spike.png")
+
+    loadSprite("jumpy", "assets/sprites/jumpy.png")
+
+    loadSprite("ghosty", "assets/sprites/ghosty.png")
+
+    loadSprite("grass", "assets/sprites/grass.png")
+}
