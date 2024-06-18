67e23028976181fd8c8a28e51bdb724f740ad1e0
sharmnten <115847442+sharmnten@users.noreply.github.com>
2024-06-18 11:40:42 -0500

    Update assets.js

---
+++ scripts/bagsLife/assets.js
@@ -0,0 +1,15 @@
+export function loadAssets(){
+    // load assets
+    loadSprite("bag", "assets/sprites/bag.png")
+    loadSprite("snow", "assets/sprites/snow.png")
+    loadSprite("coin","assets/sprites/coin.png")
+
+    loadSprite("portal", "assets/sprites/portal1.png")
+
+    loadSprite("spike", "assets/sprites/spike.png")
+
+	@@ -13,4 +13,4 @@ export function loadAssets(){
+    loadSprite("ghosty", "assets/sprites/ghosty.png")
+
+    loadSprite("grass", "assets/sprites/grass.png")
+}
