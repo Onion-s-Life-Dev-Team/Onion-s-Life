b5005e8225f9f9e912e6758d4bc868e891b9c075
sharmnten <115847442+sharmnten@users.noreply.github.com>
2024-06-18 14:26:02 +0000

    Fixed button behavior plus more

---
+++ scripts/hacker-campaign/assets.js
@@ -0,0 +1,18 @@
+export function loadAssets(){
+    // load assets
+    loadSprite("bag", "assets/sprites/bag.png")
+    loadSprite("onion","assets/sprites/onion.png")
+    loadSprite("snow", "assets/sprites/snow.png")
+    loadSprit("hacker", "assets/sprites/gigagantrum.png")
+    loadSprite("coin","assets/sprites/coin.png")
+
+    loadSprite("portal", "assets/sprites/portal1.png")
+
+    loadSprite("spike", "assets/sprites/spike.png")
+
+    loadSprite("jumpy", "assets/sprites/jumpy.png")
+
+    loadSprite("ghosty", "assets/sprites/ghosty.png")
+
+    loadSprite("grass", "assets/sprites/grass.png")
+}
