d033fd4af4581c11cf2d228c56d9170a779f306f
LagTheSystem <lagthesystem@duck.com>
2024-06-21 02:09:55 +0000

    Optimizations

--- scripts/levelOptimizer.js
+++ scripts/levelOptimizer.js
@@ -1,4 +1,7 @@
-export default function optimizeLevel(map) {
+var levelChunks;
+
+export function optimizeLevel(map) {
+    levelChunks = searchLevel(map);
     for (var i = 0; i < map.length; i++) {
         map[i] = map[i].replaceAll("=====", "  +  ");
         map[i] = map[i].replaceAll("  +    +    +    +    +  ", "            _            ");
@@ -7,7 +10,7 @@ export default function optimizeLevel(map) {
         //map[i] = map[i].replaceAll("^^^^^^^^^", "    &    ");
         //map[i] = map[i].replaceAll("^^^", " ` ");
     }
-    console.log(searchLevel(map));
+    console.log(levelChunks);
     return map;
 }
 
@@ -16,23 +19,24 @@ function searchColumn(map, column) {
     let startCoord;
     let currentChar;
     for (var i = 0; i < map.length; i++) {
-        if (i == 0) {
-            startCoord = 0;
-        } else if (i == 1) {
-            if (map[i].charAt(column) != map[i - 1].charAt(column)) {
-                startCoord = 1;
-            }
-        } else if (i == map.length - 1) {
-            if (map[i].charAt(column) == map[i - 1].charAt(column)) {
-                chunks.push({start: startCoord, end: i + 1, tile: map[i].charAt(column), col: column});
-            }
-        } else if (map[i].charAt(column) == map[i - 1].charAt(column)) {
-            console.log("Match Found");
-            if (map[i - 1].charAt(column) != map[i - 2].charAt(column)) {
-                startCoord = i - 1;
-            }
-            if(map[i].charAt(column) != map[i + 1].charAt(column)) {
-                chunks.push({start: startCoord, end: i + 1, tile: map[i].charAt(column), col: column});
+        if (map[i].charAt(column) == "=") {
+            if (i == 0) {
+                startCoord = 0;
+            } else if (i == 1) {
+                if (map[i].charAt(column) != map[i - 1].charAt(column)) {
+                    startCoord = 1;
+                }
+            } else if (i == map.length - 1) {
+                if (map[i].charAt(column) == map[i - 1].charAt(column)) {
+                    chunks.push({ start: startCoord, end: i, tile: map[i].charAt(column), col: column });
+                }
+            } else if (map[i].charAt(column) == map[i - 1].charAt(column)) {
+                if (map[i - 1].charAt(column) != map[i - 2].charAt(column)) {
+                    startCoord = i - 1;
+                }
+                if (map[i].charAt(column) != map[i + 1].charAt(column)) {
+                    chunks.push({ start: startCoord, end: i, tile: map[i].charAt(column), col: column });
+                }
             }
         }
     }
@@ -42,11 +46,31 @@ function searchColumn(map, column) {
 function searchLevel(map) {
     let chunks = [];
     for (var i = 0; i < map[0].length; i++) {
-        chunks.push(searchColumn(map, i));
+        let col = searchColumn(map, i);
+        if (col.length != 0) {
+            chunks.push(searchColumn(map, i));
+        }
     }
     return chunks;
 }
 
-function drawQuads(map) {
-    
+export function drawSprites(map) {
+    /*
+    for (var o = 0; o = levelChunks.length - 1; o++) {
+        for (var i = 0; i = levelChunks[o].length - 1; i++) {
+            console.log("Draw Sprites");
+        }
+    }*/
+    drawSprite({
+        sprite: "jumpy",
+        pos: vec2(0, 576),
+        width: 64,
+        height: 64,
+        tiled: true,
+        anchor: "bot"
+    })
+}
+
+function removeTiles(map, chunks) {
+
 }
